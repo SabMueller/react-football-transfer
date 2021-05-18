aaa04fe33027d6616bf1ded5f941b2eb091d9253
Sabrina Mueller <sabrina@famefox.de>
2021-05-18 10:02:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components/macro';
+import { useState } from 'react';
 
 function App() {
+  const initialPlayer = {
+    name: '',
+    price: '',
+    free_transfer: false,
+    club: '',
+    position: '',
+    email: '',
+  };
+
+  const [player, setPlayer] = useState(initialPlayer);
+
+  function updatePlayer(event) {
+    const fieldName = event.target.name;
+    let fieldValue = event.target.value;
+
+    if (event.target.type === 'checkbox') {
+      fieldValue = event.target.checked;
+    }
+
+    setPlayer({ ...player, [fieldName]: fieldValue });
+  }
+
+  function addPlayer(event) {
+    event.preventDefault();
+    console.log('Add This Player', player);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Headline>Add New Player</Headline>
+
+      <Form onSubmit={addPlayer}>
+        <label htmlFor="name">Player Name</label>
+        <input
+          type="text"
+          name="name"
+          onChange={updatePlayer}
+          value={player.name}
+        />
+
+        <label htmlFor="price">Transfer Price (in €)</label>
+        <input
+          type="text"
+          name="price"
+          value={player.price}
+          onChange={updatePlayer}
+          disabled={player.free_transfer}
+        />
+
+        <label htmlFor="free_transfer">Free transfer?</label>
+        <input
+          type="checkbox"
+          name="free_transfer"
+          value={player.free_transfer}
+          onChange={updatePlayer}
+          checked={player.free_transfer}
+          disabled={player.price.length >= 1}
+        />
+
+        <label htmlFor="club">Club</label>
+        <select
+          name="club"
+          id="club"
+          value={player.club}
+          onChange={updatePlayer}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="">Select the Club</option>
+          <option value="fc_bayern">FC Bayern</option>
+          <option value="st_pauli">St Pauli</option>
+          <option value="fc_arsenal">FC Arsenal</option>
+          <option value="man_city">Manchester City</option>
+          <option value="fc_barcelona">FC Barcelona</option>
+          <option value="psg">Paris Saint-Germain</option>
+          <option value="fc_liverpool">FC Liverpool</option>
+          <option value="juventus">Juventus Turin</option>
+        </select>
+
+        <label htmlFor="position">Position</label>
+        <Position>
+          <input
+            type="radio"
+            name="position"
+            value="striker"
+            onChange={updatePlayer}
+            checked={player.position === 'striker'}
+          />{' '}
+          Striker
+          <input
+            type="radio"
+            name="position"
+            value="midfield"
+            onChange={updatePlayer}
+            checked={player.position === 'midfield'}
+          />{' '}
+          Midfield
+          <input
+            type="radio"
+            name="position"
+            value="defence"
+            onChange={updatePlayer}
+            checked={player.position === 'defence'}
+          />{' '}
+          Defence
+          <input
+            type="radio"
+            name="position"
+            value="goalie"
+            onChange={updatePlayer}
+            checked={player.position === 'goalie'}
+          />{' '}
+          Goalie
+        </Position>
+        <label htmlFor="email">Contact</label>
+        <input type="email" name="email" onChange={updatePlayer} />
+        <Buttons>
+          <Button isPrimary type="submit">
+            Add
+          </Button>
+          <Button type="cancel">Cancel</Button>
+        </Buttons>
+      </Form>
     </div>
   );
 }
 
 export default App;
+
+const Headline = styled.h1`
+  font-family: 'Reggae One', cursive;
+  text-align: center;
+  color: #9e37a2;
+`;
+
+const Form = styled.form`
+  display: grid;
+  gap: 0.5rem;
+  margin: 0 auto;
+  width: 16rem;
+
+  label {
+    font-weight: bold;
+    font-family: sans-serif;
+    margin-bottom: 0.5rem;
+  }
+  input,
+  select {
+    display: flex;
+    font-size: 1.125rem;
+    font-family: sans-serif;
+    margin-bottom: 1.5rem;
+  }
+  input[type='checkbox'],
+  input[type='radio'] {
+    transform: scale(1.4);
+  }
+`;
+
+const Position = styled.section`
+  display: flex;
+  gap: 1rem;
+`;
+const Buttons = styled.section`
+  display: flex;
+  gap: 1rem;
+  padding: 2rem 0;
+`;
+const Button = styled.button`
+  padding: 1rem;
+  border-radius: 0.5rem;
+  background: none;
+  cursor: pointer;
+  border: none;
+  font-size: 1.2rem;
+  width: 10rem;
+  background: ${(props) => (props.isPrimary ? '#9e37a2' : 'none')};
+  color: white;
+`;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+
+body {
+background-image: url('https://gmedia.playstation.com/is/image/SIEPDC/fifa-21-background-block-desktop-01-en-19jun20?$native$');
+background-repeat: no-repeat;
+background-size: cover;
+color: white; 
+
+}
+`;
